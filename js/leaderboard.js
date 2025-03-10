e3db0781b6e768f19d16718a761a8f6740402391
Angelo Iodice <therecruiterhg88@gmail.com>
2025-03-10 23:49:45 +0000

    .

--- js/leaderboard.js
+++ js/leaderboard.js
@@ -29,79 +29,6 @@ document.addEventListener('DOMContentLoaded', () => {
     document.body.appendChild(overlay);
     let isPanelVisible = false;
 
-    // Example leaderboard data for different challenges
-    const leaderboardData = {
-        total: [
-            { player: "Player 001", score: 70 },
-            { player: "Player 002", score: 60 },
-            { player: "Player 003", score: 50 },
-            { player: "Player 004", score: 200 },
-            { player: "Player 005", score: 250 },
-            { player: "Player 006", score: 110 },
-            { player: "Player 007", score: 10 },
-            { player: "Player 008", score: 90 },
-            { player: "Player 009", score: 400 },
-            { player: "Player 010", score: 370 },
-            { player: "Player 011", score: 70 },
-            { player: "Player 012", score: 40 },
-            { player: "Player 013", score: 30 },
-            { player: "Player 014", score: 20 },
-            { player: "Player 015", score: 10 }
-        ],
-        challenge1: [
-            { player: "Player 001", score: 30 },
-            { player: "Player 002", score: 50 },
-            { player: "Player 003", score: 40 },
-            { player: "Player 004", score: 200 },
-            { player: "Player 005", score: 250 },
-            { player: "Player 006", score: 110 },
-            { player: "Player 007", score: 10 },
-            { player: "Player 008", score: 90 },
-            { player: "Player 009", score: 400 },
-            { player: "Player 010", score: 370 },
-            { player: "Player 011", score: 70 },
-            { player: "Player 012", score: 40 },
-            { player: "Player 013", score: 30 },
-            { player: "Player 014", score: 20 },
-            { player: "Player 015", score: 10 }
-        ],
-        /*challenge2: [
-            { player: "Player 001", score: 40 },
-            { player: "Player 002", score: 30 },
-            { player: "Player 003", score: 30 },
-            { player: "Player 004", score: 200 },
-            { player: "Player 005", score: 250 },
-            { player: "Player 006", score: 110 },
-            { player: "Player 007", score: 10 },
-            { player: "Player 008", score: 90 },
-            { player: "Player 009", score: 400 },
-            { player: "Player 010", score: 370 },
-            { player: "Player 011", score: 70 },
-            { player: "Player 012", score: 40 },
-            { player: "Player 013", score: 30 },
-            { player: "Player 014", score: 20 },
-            { player: "Player 015", score: 10 }
-        ],*/
-        /*challenge3: [
-            { player: "Player 001", score: 60 },
-            { player: "Player 002", score: 40 },
-            { player: "Player 003", score: 50 },
-            { player: "Player 004", score: 200 },
-            { player: "Player 005", score: 250 },
-            { player: "Player 006", score: 110 },
-            { player: "Player 007", score: 10 },
-            { player: "Player 008", score: 90 },
-            { player: "Player 009", score: 400 },
-            { player: "Player 010", score: 370 },
-            { player: "Player 011", score: 70 },
-            { player: "Player 012", score: 40 },
-            { player: "Player 013", score: 30 },
-            { player: "Player 014", score: 20 },
-            { player: "Player 015", score: 10 }
-        ]*/
-    };
-
-
     // Function to fetch leaderboard data from Firebase
     async function fetchLeaderboardData(tab) {
         try {
@@ -180,7 +107,7 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 
     // Event listener to switch between leaderboard tabs
-    const tabButtons = document.querySelectorAll('.tab-button');
+    const tabButtons = document.getElementById('leaderboard-tab-button');
     tabButtons.forEach(button => {
         button.addEventListener('click', (event) => {
             const selectedTab = event.target.dataset.tab;
@@ -192,7 +119,7 @@ document.addEventListener('DOMContentLoaded', () => {
             event.target.classList.add('active');
             
             // Generate the leaderboard for the selected tab
-            generateLeaderboard(selectedTab);
+            if(isLeaderboardVisible) generateLeaderboard(selectedTab);
         });
     });
 });
