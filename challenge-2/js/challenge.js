e124b03048d16f5fdf0ecd64369a9ebba8e2105e
Angelo Iodice <therecruiterhg88@gmail.com>
2025-03-19 18:06:36 +0000

    .

--- challenge-2/js/challenge.js
+++ challenge-2/js/challenge.js
@@ -86,7 +86,7 @@ beginChallengeBtn.addEventListener('click', () => {
   const selectedPlayerNumber = playerNumberSelect.value;
 
   // Ensure the selected player number is from the dropdown list
-  const validPlayers = ["002", "016", "018", "019", "023", "026", "051", "054", "057", "065", "068", "077", "107", "120", "130", "135", "136", "141", "182", "183", "186", "187", "188", "194", "221", "222", "226", "229", "232", "243", "273", "278", "286", "287", "297", "301", "339", "343"];
+  const validPlayers = ["001", "002", "016", "018", "019", "023", "026", "051", "054", "057", "065", "068", "077", "107", "120", "130", "135", "136", "141", "182", "183", "186", "187", "188", "194", "221", "222", "226", "229", "232", "243", "273", "278", "286", "287", "297", "301", "339", "343"];
 
   if (!validPlayers.includes(selectedPlayerNumber)) {
     alert("Invalid player number! You're trying to mess up the scoreboard... xD");
